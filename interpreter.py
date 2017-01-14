f26954cb7b044adb36dd6b3a921e48b746626021
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2017-01-13 19:26:38 -0500

    added parser to interpreter

--- interpreter.py
+++ interpreter.py
@@ -2,3 +2,39 @@
 
 def tokenize(input):
 	return input.replace('(', ' ( ').replace(')', ' ) ').split()
+
+
+def read_tokens(tokens):
+	if (len(tokens)) == 0:
+		raise SyntaxError('unexpected EOF while reading')
+	token = tokens.pop(0)
+	if ('(' == token):
+		inside = []
+		while tokens[0] != ')':
+			inside.append(read_tokens(tokens))
+		tokens.pop(0)
+		return inside
+	elif (')' == token):
+		raise SyntaxError('unexpected closed parenthesis')
+	else:
+		return atomize(token)
+
+
+def atomize(token):
+	try: return int(token)
+	except ValueError:
+		try: return float(token)
+		except ValueError:
+			return token
+
+
+def parse(input):
+	return read_tokens(tokenize(input))
+
+
+def testing():
+	program = "(begin (define t 42) (* pi (* t t)))"
+	print(parse(program))
+
+
+testing()
